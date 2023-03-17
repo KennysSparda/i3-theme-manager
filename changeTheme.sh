264ce2321e6456e572aa036fd7766fa934491993
KennysSparda <kennykysv@gmail.com>
2023-03-17 13:05:26 -0300

    Initial commit

---
+++ changeTheme.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/bash
+
+themes="/home/swygax/.config/mytheme/themes"
+function setWallpaper(){
+  sh ./config
+}
+
+function changeToRed(){
+  cp $themes/red/config ~/.config/i3/
+  cp $themes/red/image ~/.config/wallpaper/
+  setWallpaper
+  i3-msg reload
+  i3-msg restart
+}
+
+function changeToBlue(){
+  cp $themes/blue/config ~/.config/i3/
+  cp $themes/blue/image ~/.config/wallpaper/
+  setWallpaper
+  i3-msg reload
+  i3-msg restart
+}
+
+function changeToGreen(){
+  cp $themes/green/config ~/.config/i3/
+  cp $themes/green/image ~/.config/wallpaper/
+  setWallpaper
+  i3-msg reload
+  i3-msg restart
+}
+
+function changeToPurple(){
+  cp $themes/purple/config ~/.config/i3/
+  cp $themes/purple/image ~/.config/wallpaper/
+  setWallpaper
+  i3-msg reload
+  i3-msg restart
+}
+
+menu() {
+  option=$(zenity --list --height="300" --title="Change theme" --column="Themes" "Red" "Blue" "Green" "Purple" "Exit")
+
+  case $option in
+    Red)
+      changeToRed
+      ;;
+    Blue)
+      changeToBlue
+      ;;
+    Green)
+      changeToGreen
+      ;;
+    Purple)
+      changeToPurple
+      ;;
+    Exit)
+      exit -1
+      ;;
+    *)
+      exit -1
+      ;;
+  esac
+}
+menu
