bdcc6c4841ec37d79da3ca9dc777b07aa9b7feb0
KennysSparda <kennykysv@gmail.com>
2026-07-26 02:52:58 -0300

    refactor(core): migrate theme selection menu from zenity to dmenu

--- i3-theme-manager.sh
+++ i3-theme-manager.sh
@@ -4,38 +4,50 @@ theme_manager_dir=~/.config/i3-theme-manager
 themes_dir=$theme_manager_dir/themes
 wallpaper_dir=~/.config/wallpaper
 
-# Verificar se o diretório existe
-mkdir -p $wallpaper_dir
-
-function setWallpaperAndReload() {
-  nitrogen --set-zoom-fill "$wallpaper_dir/image"
-  $theme_manager_dir/monitor-sizes.sh
-  i3-msg reload
-  i3-msg restart
-}
+mkdir -p "$wallpaper_dir"
 
 function changeTheme() {
   theme=$1
+  
+  # 1. Gera os templates
   $theme_manager_dir/monitor-sizes.sh
-  $theme_manager_dir/apply-color.sh $theme
+  $theme_manager_dir/apply-color.sh "$theme"
+  
+  # 2. Atualiza a config do i3 e o wallpaper
   cp /tmp/i3_config_template_2 ~/.config/i3/config
-  cp "$themes_dir/$theme/image" "$wallpaper_dir/"
-  setWallpaperAndReload
+  cp "$themes_dir/$theme/image" "$wallpaper_dir/image"
+  
+  # 3. Aplica o papel de parede e reinicia o i3 apenas UMA vez
+  nitrogen --set-zoom-fill "$wallpaper_dir/image"
+  i3-msg restart
 }
 
 menu() {
-  options=("red" "blue" "green" "purple" "Exit") # Add before "purple" the name of your theme in quotes, like this: options=("red" "blue" "green" "purple" "my-new-theme" "Exit")
-  theme=$(zenity --list --height="300" --title="Change theme" --column="Themes" "${options[@]}")
+  # LÊ AS CORES ATUAIS DIRETO DO SEU I3 CONFIG EM USO
+  c_primary=$(grep 'set $primary-color' ~/.config/i3/config | awk '{print $3}')
+  c_secondary=$(grep 'set $secondary-color' ~/.config/i3/config | awk '{print $3}')
+  c_tertiary=$(grep 'set $tertiary-color' ~/.config/i3/config | awk '{print $3}')
+
+  # FALLBACK DE SEGURANÇA: se o config estiver em branco ou quebrado
+  c_primary=${c_primary:-"#005577"}
+  c_secondary=${c_secondary:-"#eeeeee"}
+  c_tertiary=${c_tertiary:-"#222222"}
+
+  options="red\nblue\ngreen\npurple\nExit"
+  
+  # Abre o dmenu usando as cores que já estão no seu painel
+  theme=$(echo -e "$options" | dmenu -i -p "Theme:" \
+    -nb "$c_tertiary" \
+    -nf "$c_secondary" \
+    -sb "$c_primary" \
+    -sf "$c_tertiary")
 
   case $theme in
-    red|blue|green|purple) # Add here, the name of your theme without quotes, like this: red|blue|green|purple|my-new-theme)
+    red|blue|green|purple)
       changeTheme "$theme"
       ;;
-    Exit)
-      exit -1
-      ;;
     *)
-      exit -1
+      exit 0
       ;;
   esac
 }
